f1d6d99da87eeff9a6fb20fd7fc5cbb7125988b5
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2023-05-04 18:18:07 +0530

    Retrofit in HomeF

--- app/src/main/java/com/example/easyfood/activities/MainActivity.kt
+++ app/src/main/java/com/example/easyfood/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.easyfood
+package com.example.easyfood.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.example.easyfood.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigation : BottomNavigationView = findViewById(R.id.btm_nav)
-        val navController : NavController = Navigation.findNavController(this,R.id.host_fragment)
+        val navController : NavController = Navigation.findNavController(this, R.id.host_fragment)
 
         NavigationUI.setupWithNavController(bottomNavigation,navController)
     }
